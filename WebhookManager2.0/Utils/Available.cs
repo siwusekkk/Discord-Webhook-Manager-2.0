79363032799c37966a670313865c9254f72e67ab
siwusekkk <80574029+siwusekkk@users.noreply.github.com>
2022-11-29 20:27:57 +0100

    Add files via upload

---
+++ WebhookManager2.0/Utils/Available.cs
@@ -0,0 +1,20 @@
+﻿using Siticone.Desktop.UI.WinForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebhookManager2._0.Utils
+{
+    class Available
+    {
+        public static void SetAvailable(SiticoneTextBox target)
+        {
+            Color color = Color.FromArgb(131, 8, 255);
+            target.BorderColor = color;
+            target.HoverState.BorderColor = color;
+            target.FocusedState.BorderColor = color;
+        }
+    }
+}
---
+++ WebhookManager2.0/Utils/Unavailable.cs
@@ -0,0 +1,19 @@
+﻿using Siticone.Desktop.UI.WinForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebhookManager2._0.Utils
+{
+    class Unavailable
+    {
+        public static void SetUnavailable(SiticoneTextBox target)
+        {
+            target.BorderColor = Color.Gray;
+            target.HoverState.BorderColor = Color.Gray;
+            target.FocusedState.BorderColor = Color.Gray;
+        }
+    }
+}
